4cfae75e79f3322e5a7590d65b5604b0f83065b0
bbq <uestcwzq@gmail.com>
2016-05-12 22:13:30 +0800

    1.[core] TorrentBroadcast local save Block into BlockManager 2.[core] TODO split Broadcast into Chunks

--- core/src/main/scala/org/scu/spark/broadcast/TorrentBroadcast.scala
+++ core/src/main/scala/org/scu/spark/broadcast/TorrentBroadcast.scala
@@ -1,13 +1,43 @@
 package org.scu.spark.broadcast
 
-import org.scu.spark.Logging
+import org.scu.spark.rdd.SparkException
+import org.scu.spark.storage.{StorageLevel, BroadcastBlockId}
+import org.scu.spark.{SparkEnv, SparkConf, Logging}
 
 import scala.reflect.ClassTag
 
 /**
+ * 按照比特流的的方式实现。运行机制：driver端将需要序列化的对象进行分片，然后将分片存储到blockManager中。
+ * 每个executor从本地blockManager获取数据，如果数据不存在，那么就通过远程来去的方式以分片为单位从driver或者其他节点获取。
+ * 当获取到一个分片的时候，将分片放入自身的blockManager，可以提供给其他executors。（这种方式避免了driver成为性能瓶颈。）
  * Created by bbq on 2016/5/5
  */
 private[spark] class TorrentBroadcast[T:ClassTag](obj:T,id:Long) extends Broadcast[T](id) with Logging with Serializable{
+
+  private var blockSize : Int = _
+
+  private def setConf(conf:SparkConf): Unit = {
+    //TODO code compress
+    /**每个数据块的大小，单位是Byte*/
+    blockSize = conf.get("spark.broadcast.blockSize","4").toInt * 1024 * 1024
+  }
+
+  setConf(SparkEnv.env.conf)
+
+  private val broadcasId = BroadcastBlockId(id)
+
+
+  private def writeBlocks(value:T):Int={
+    import StorageLevel._
+    val blockManager = SparkEnv.env.blockManager
+    /**在本地存一份*/
+    if(!blockManager.putSingle(broadcasId,value,MEMORY_ONLY,false)){
+      throw new SparkException(s"Failed to stroe $broadcasId in BlockManager")
+    }
+
+    ???
+
+  }
 
 }
 
